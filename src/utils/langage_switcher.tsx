9da66e331078a7e4d8d7f01a2a14c9a7351b55bd
abel <abel.aubron@gmail.com>
2026-05-04 11:18:01 +0200

    puting SEO xml in public

--- src/utils/langage_switcher.tsx
+++ src/utils/langage_switcher.tsx
@@ -13,12 +13,12 @@ const LanguageSwitcher = () => {
   return (
       <div className="flex items-center bg-blue/10 backdrop-blur-md rounded-full border border-white/10 shadow-lg w-max">
         <motion.button whileHover={{ scale: 1.03 }} className="py-0.5" onClick={() => switchLanguage()}>
-          <span className={`px-4 py-1 text-center rounded-full transition-all ${
+          <span className={`px-4 py-1.5 text-center rounded-full transition-all ${
             currentLang === "en"
               ? "bg-gradient-to-r from-primary to-secondary text-white"
               : "text-gray-300 hover:text-white"
           }`}>🇬🇧</span>
-          <span className={`px-4 py-1 text-center rounded-full transition-all ${
+          <span className={`px-4 py-1.5 text-center rounded-full transition-all ${
             currentLang === "fr"
               ? "bg-gradient-to-r from-primary to-secondary text-white"
               : "text-gray-300 hover:text-white"
